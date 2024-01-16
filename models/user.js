669a51de7f96779efc4ad65949ff8cfac612c3da
Sameera Pasan <as2020928@sci.sjp.ac.lk>
2024-01-16 13:07:10 +0530

    make it better

--- models/user.js
+++ models/user.js
@@ -1,31 +1,31 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  todos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Todo",
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    todos:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Todo",
-        }
-    ],
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
